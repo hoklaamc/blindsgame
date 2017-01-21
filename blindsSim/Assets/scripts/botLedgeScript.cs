83e086e910fa9028f9a1a267f5f5da2fc585cf7d
Jeff Teng <jeff@medella.ca>
2017-01-21 12:41:54 -0500

    updated to reflect latest files

---
+++ blindsSim/Assets/scripts/botLedgeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class botLedgeScript : MonoBehaviour {
+
+	public GameObject blinds1;
+	public GameObject blinds2;
+	public GameObject blinds3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("Fire1")) 
+		{
+			blinds1.GetComponent<Rigidbody2D> ().gravityScale = -1;
+			blinds2.GetComponent<Rigidbody2D> ().gravityScale = -1;
+			blinds3.GetComponent<Rigidbody2D> ().gravityScale = -1;
+		} 
+		else if (Input.GetButtonUp("Fire1"))
+		{
+			blinds1.GetComponent<Rigidbody2D> ().gravityScale = 1;	
+			blinds2.GetComponent<Rigidbody2D> ().gravityScale = 1;
+			blinds3.GetComponent<Rigidbody2D> ().gravityScale = 1;
+		}		
+	}
+}
