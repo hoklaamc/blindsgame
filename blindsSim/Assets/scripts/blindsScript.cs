7dd6dd1b9b8138bf81370a892a591d1b00457071
hoklaamc <h199875@gmail.com>
2017-01-21 16:58:57 -0500

    Added collision sounds for blinds

---
+++ blindsSim/Assets/scripts/blindsScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blindsScript : MonoBehaviour {
+
+    new AudioSource audio;
+
+	// Use this for initialization
+	void Start () {
+        // Collision sound
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.relativeVelocity.magnitude > 3) {
+            // Scales volume to velocity of collision
+            audio.volume = collision.relativeVelocity.magnitude / 20;
+            audio.Play();
+        }
+    }
+}
