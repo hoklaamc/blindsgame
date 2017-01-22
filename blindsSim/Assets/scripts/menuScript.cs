d98f46286c9f856680d420d27d899ab764cae0d9
Jeff Teng <jeff@medella.ca>
2017-01-22 13:39:39 -0500

    added title page

--- blindsSim/Assets/scripts/menuScript.cs
+++ blindsSim/Assets/scripts/menuScript.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class menuScript : MonoBehaviour {
 	
 	public GameObject menuCanvas;
+	public GameObject blinds;
+
+	void Start() 
+	{
+		//blinds.SetActive (false);
+	}
 
 	public void onStartGame()
 	{
 		//Debug.Log ("Start pressed!");
 		menuCanvas.SetActive (false);
+		//blinds.SetActive (true);
 	}
 }
